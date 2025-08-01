abecd07e34e86688ff4e22152e9e7a2b975861e9
Rajesh Bandaru <rajeshkumarsun56@gmail.com>
2025-08-01 12:35:12 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="#" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                  <a href="/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
               </li>
               <li>
-                  <a href="#" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                  <a href="/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
               </li>
               <li>
-                  <a href="#" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
+                  <a href="/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
               </li>
               <li>
                   <a href="https://www.youtube.com/channel/UCuH_UB-L_PaVQSBoOqp0cqw" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
